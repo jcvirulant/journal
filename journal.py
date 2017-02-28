bbebddec7dfab2d15624d7b65ceca700bf9202fa
Melissa abraham <Melissa@Melissas-MacBook-Pro.local>
2017-02-28 18:31:58 -0400

    minor updates

--- journal.py
+++ journal.py
@@ -5,10 +5,22 @@ db = SqliteDatabase('diary.db')
 
 class Entry(Model):
     # content
+    db.create('journal.db', safe=True)
+    db.create_tables(Entry, safe=True)
+    )
     # timestamp
 
     class Meta:
         database = db
+
+
+menu = OrderedDict([
+    ('a', add_entry)
+])
+
+
+def initialize():
+    """Initialize the App"""
 
 
 def menu_loop():
@@ -21,11 +33,19 @@ def add_entry():
 
 def view_entries():
     """View previous entries."""
+    print('d) delete entry')
+
+
+    next_action = input('Action: [Nqd] ').lower().strip()
+    if next_action == input()
 
 
 def delete_entry(entry):
     """Delete an entry."""
+    if input('Are you sure? [yN] ').lower() == 'y':
+        entry.delete_instance()
 
 
 if __name__ == '__main__':
+    initialize()
     menu_loop()
